efbb04dcdd8bef0cb0c733092c38612570620897
yoavmmn <yoavmmn@gmail.com>
2018-04-25 03:19:20 +0300

    added argument and error handling

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const { readFileSync } = require('fs');
 const { argv } = process;
 const file = argv[2];
 
+if (!file) {
+  console.log('Usage: asynode path/to/file.js');
+  process.exit(0);
+}
+
 // read file content
-const fileContent = readFileSync(file, 'utf8');
+let fileContent;
+
+try {
+  fileContent = readFileSync(file, 'utf8');
+} catch (e) {
+  console.log(`Error: "${file}" doesn't exist`);
+  process.exit(1);
+}
 
 // evaluate JavaScript code and run as async IIFE
 eval(`(async function() {
